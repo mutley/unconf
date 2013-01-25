e0651996199caafb30b1c60b23343d4fec144397
Matthew Nielsen <xunker@pyxidis.org>
2013-01-24 21:04:44 -0700

    "Initial Commit" is the new black.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Unconf::Application.config.secret_token = 'd147f2850a3f23a857c002a990885c24079f09f4d2562c822f12a14ffde0ce631f12aa9fd09b53383f200785d6f84812746029fdcb5763770859e2e8e9a8d877'
