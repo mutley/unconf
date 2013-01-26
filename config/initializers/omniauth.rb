d00437f95c59800943670110ac961fabe711a432
Matthew Nielsen <xunker@pyxidis.org>
2013-01-26 09:19:27 -0700

    switching back, testing scope

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   if Rails.env.production?
     provider :youtube, ENV['YOUTUBE_KEY'], ENV['YOUTUBE_SECRET'], {
-      :access_type => 'online', :approval_prompt => ''#,
-      # :scope => "https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/userinfo.email"
+      :access_type => 'online', :approval_prompt => '',
+      :scope => "https://www.googleapis.com/auth/youtube.readonly https://www.googleapis.com/auth/userinfo.email"
     }
   else
     provider :youtube, "1072289531779-rnhstpcig8a5p2chrj6mmldfmvk75aga.apps.googleusercontent.com", "LRA2e0G8asuQ5CFtgHmN7MDn", {
